3a0efc311bc3fb49883e688fe37d35d095092438
thmsaguiar <thvicente110100@gmail.com>
2021-10-12 18:30:04 -0300

    adicionando mensagem de start e gameover

--- Sprite.js
+++ Sprite.js
@@ -5,10 +5,22 @@ function Sprite(x, y, width, height){
 	this.height = height;
 
 	this.drawing = function(){
-		ctx.drawImage(img, this.x, this.y, this.width, this.height);
+		ctx.drawImage(img, this.x, this.y-5, this.width, this.height);
 	}
 	this.drawingBnc = function(x, y){
 		ctx.drawImage(imgBnc, x, y, 50, 50);
+	}
+	this.drawingStr = function(x, y){
+		ctx.drawImage(imgStr, x, y, 500, 300);
+	}
+	this.drawingFl = function(x, y){
+		ctx.drawImage(imgFl, x-5, y-5, 610, 55);
+	}
+	this.drawingGo = function(x, y){
+		ctx.drawImage(imgGo, x, y, 500, 300);
+	}
+	this.drawingRcd = function(x, y){
+		ctx.drawImage(imgRcd, x, y, 500, 300);
 	}
 }
 
